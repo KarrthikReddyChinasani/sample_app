c5cf6c92ebe6c6a739784ead3c4aac285522adc3
Karthik Reddy <karthikreddy.chinasani1995@gmail.com>
2020-01-16 12:25:11 +0530

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user, only: %i[edit update index destroy]
+  before_action :correct_user, only: %i[edit update]
   before_action :admin_user, only: :destroy
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
@@ -21,7 +24,7 @@ class UsersController < ApplicationController
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
+    flash[:success] = 'User deleted'
     redirect_to users_url
   end
 
@@ -29,21 +32,21 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      flash[:info] = 'Please check your email to activate your account.'
       redirect_to root_url
     else
-      flash[:error] = "Something went wrong"
-      render "new"
+      flash[:error] = 'Something went wrong'
+      render 'new'
     end
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = 'Profile updated'
       redirect_to @user
     else
-      render "edit"
+      render 'edit'
     end
   end
 
@@ -52,14 +55,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
-  end
-
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
   end
 
   # Confirms the correct user.
---
+++ app/models/micropost.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Micropost < ApplicationRecord
+  belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  has_one_attached :image
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
+                                    message: 'must be a valid image format' },
+                    size: { less_than: 5.megabytes,
+                            message: 'should be less than 5MB' }
+
+  def display_image
+    image.variant(resize_to_limit: [300, 300])
+  end
+end
